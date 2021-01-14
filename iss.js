235c9e0d314195e37b4c124616fdb6717e959e78
Billy McKinnon <70711194+Billex87@users.noreply.github.com>
2021-01-14 14:01:01 -0800

    ISS Spotter IV

--- iss.js
+++ iss.js
@@ -1,1 +1,53 @@
+const request = require('request');
 
+const fetchISSFlyOverTimes = function (coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    callback(error, JSON.parse(body).response);
+  })
+}
+const fetchMyIP = function (callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    // inside the request callback ... error can be set if invalid domain, user is offline, etc.
+    if (error) return callback(error, null);
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
+    callback(error, JSON.parse(body).ip);
+  });
+
+};
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    callback(error, {
+      latitude: JSON.parse(body).latitude,
+      longitude: JSON.parse(body).longitude
+    });
+  });
+};
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+    module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation }
